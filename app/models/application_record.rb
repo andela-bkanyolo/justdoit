6e6a91af818fc0d431f73630984a09e6aebcb4f2
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-29 21:32:19 +0300

    ch(models): add user model validations

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\.]+[\w+]\.[a-z]+\z/i
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  validates :password,
+            presence: true,
+            confirmation: true,
+            length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :email,
+            uniqueness: true,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL }
+  has_many :bookings
+
+  has_secure_password
 end
