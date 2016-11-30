297e22a4133a763d2fcc56ebc4ab9f9bb6e84ce8
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-11-30 22:05:26 +0300

    tests(login): refactor user login specs

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
+  describe 'POST #login' do
+    context 'when params are valid' do
+      before(:each) do
+        user = create(:user, password: 'password')
+        post :login, params: {
+          email: user.email,
+          password: 'password'
+        }
+      end
 
+      it_behaves_like('a http response', 200, 'Successfully logged in')
+
+      it 'returns an authentication token' do
+        expect(json['auth_token']).not_to be_nil
+      end
+    end
+
+    context 'when params are invalid' do
+      before(:each) do
+        post :login, params: {}
+      end
+
+      it_behaves_like(
+        'a http response',
+        401,
+        'Log in email or password incorrect'
+      )
+    end
+  end
 end
