b61028e6ef9ca7fe9559bdd745db9b571b5664db
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-12-01 12:41:20 +0300

    tests(authenticate): add logout specs

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe AuthenticationController, type: :controller do
         }
       end
 
-      it_behaves_like('a http response', 200, 'Successfully logged in')
+      it_behaves_like('a http response', 200, Messages.user_logged_in)
 
       it 'returns an authentication token' do
         expect(json['auth_token']).not_to be_nil
@@ -20,14 +20,33 @@ RSpec.describe AuthenticationController, type: :controller do
 
     context 'when params are invalid' do
       before(:each) do
-        post :login, params: {}
+        post :login
       end
 
-      it_behaves_like(
-        'a http response',
-        401,
-        'Log in email or password incorrect'
-      )
+      it_behaves_like('a http response', 401, Messages.user_not_logged_in)
+    end
+  end
+
+  describe 'GET #logout' do
+    let(:user) { create(:user) }
+
+    context 'when authorization succeeds' do
+      before(:each) do
+        get :logout, params: {test: "test"}
+        request.headers["authorization"] = auth_headers["authorization"]
+      end
+
+      it {binding.pry}
+
+      it_behaves_like('a http response', 200, Messages.user_logged_out)
+    end
+
+    context 'when authorization fails' do
+      before(:each) do
+        get :logout
+      end
+
+      it_behaves_like('a http response', 401, Messages.missing_token)
     end
   end
 end
