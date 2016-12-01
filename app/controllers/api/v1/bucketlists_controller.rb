49767e21603855e3bc056a64d22bb9b7af113825
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-12-01 20:02:50 +0300

    ft(index): add index action for bucketlists

--- app/controllers/api/v1/bucketlists_controller.rb
+++ app/controllers/api/v1/bucketlists_controller.rb
@@ -1,8 +1,14 @@
 module Api
   module V1
     class BucketlistsController < ApplicationController
+      before_action :set_bucket_lists
+
+      def index
+        render_json(paginate)
+      end
+
       def create
-        bucketlist = @current_user.bucketlists.create!(bucketlist_params)
+        bucketlist = @bucketlists.create!(bucketlist_params)
         render_json(bucketlist, :created)
       end
 
@@ -10,6 +16,14 @@ module Api
 
       def bucketlist_params
         params.permit(:name)
+      end
+
+      def set_bucket_lists
+        @bucketlists = current_user.bucketlists
+      end
+
+      def paginate
+        @bucketlists.paginate(params[:limit], params[:page]) if @bucketlists
       end
     end
   end
