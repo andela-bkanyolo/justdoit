1accf44ae4edd28c370546d8d843fbd5f65d24cf
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-12-01 21:56:22 +0300

    tests(querying): refactor query tests

--- spec/requests/api/v1/bucketlists/list_bucketlists_spec.rb
+++ spec/requests/api/v1/bucketlists/list_bucketlists_spec.rb
@@ -39,5 +39,32 @@ RSpec.describe 'List all bucketlists', type: :request do
     end
   end
 
+  context 'when the query provided matches' do
+    let(:query) { create(:bucketlist, user: user) }
+    let(:params) { { q: query.name } }
+    it 'finds and returns queried bucket' do
+      expect(json.map { |bucketlist| bucketlist['name'] }).to include query.name
+    end
+  end
+
+  context 'when the query provided does not match' do
+    let(:params) { { q: Faker::Code.asin } }
+
+    it 'finds and returns zero items' do
+      expect(json).to be_empty
+    end
+  end
+
+  context 'when the user has no bucketlists' do
+    before(:each) do
+      user.bucketlists.destroy_all
+      get "/bucketlists", params: params, headers: header
+    end
+
+    it 'returns zero items' do
+      expect(json).to be_empty
+    end
+  end
+
   it_behaves_like 'an unathorized response'
 end
