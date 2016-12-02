c9e5be6d868b95d3e19898249072a7ebde404712
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-12-02 08:39:21 +0300

    ch(models): add migration for item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201111707) do
+ActiveRecord::Schema.define(version: 20161202053630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20161201111707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bucketlists_on_user_id", using: :btree
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "bucketlist_id"
+    t.boolean  "done",          default: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["bucketlist_id"], name: "index_items_on_bucketlist_id", using: :btree
   end
 
   create_table "tokens", force: :cascade do |t|
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 20161201111707) do
   end
 
   add_foreign_key "bucketlists", "users"
+  add_foreign_key "items", "bucketlists"
   add_foreign_key "tokens", "users"
 end
