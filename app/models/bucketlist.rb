9a2c8ed4827b6a0d725b66f0fd65eaee8463f644
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-12-02 08:54:08 +0300

    ch(models): add item model implementation

--- app/models/bucketlist.rb
+++ app/models/bucketlist.rb
@@ -4,6 +4,7 @@ class Bucketlist < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :user
+  has_many :items, dependent: :destroy
 
   def self.search(query)
     where("name ILIKE ?", "%#{query}%")
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  extend Paginate
+
+  validates :name, presence: true
+
+  belongs_to :bucketlist
 end
