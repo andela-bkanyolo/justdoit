ee4e3c657a4d1776cbb600e9ea74729dd3d370d4
Bernard Kanyolo <bernard.kanyolo@andela.com>
2016-12-02 14:43:00 +0300

    ch: migrate to coveralls

--- app/models/bucketlist.rb
+++ app/models/bucketlist.rb
@@ -1,12 +1,9 @@
 class Bucketlist < ApplicationRecord
   extend Paginate
+  extend Searchable
 
   validates :name, presence: true
 
   belongs_to :user
   has_many :items, dependent: :destroy
-
-  def self.search(query)
-    where("name ILIKE ?", "%#{query}%")
-  end
 end
